4e7b2f7d52347273cacd9124a2b3769fa0e6b7fb
underground0930 <go.nishiduka.1985@gmail.com>
2024-03-12 12:00:01 +0900

    feature_mappingsのマイグレーションを作成

---
+++ db/migrate/20240312025503_create_feature_mappings.rb
@@ -0,0 +1,11 @@
+class CreateFeatureMappings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :feature_mappings do |t|
+      t.references :features, null: false, foreign_key: true
+      t.references :space, null: false, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :feature_mappings, [:feature_id, :space_id], unique: true
+  end
+end
