7050a62c34ca50931454156b7cc7fb7740d28d65
underground0930 <go.nishiduka.1985@gmail.com>
2024-03-12 12:13:27 +0900

    rails db migrate

---
+++ db/schema.rb
@@ -0,0 +1,91 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2024_03_12_030249) do
+  create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", charset: "utf8mb4", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "feature_mappings", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "feature_id", null: false
+    t.bigint "space_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["feature_id", "space_id"], name: "index_feature_mappings_on_feature_id_and_space_id", unique: true
+    t.index ["feature_id"], name: "index_feature_mappings_on_feature_id"
+    t.index ["space_id"], name: "index_feature_mappings_on_space_id"
+  end
+
+  create_table "features", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "space_type_mappings", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "space_type_id", null: false
+    t.bigint "space_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["space_id"], name: "index_space_type_mappings_on_space_id"
+    t.index ["space_type_id", "space_id"], name: "index_space_type_mappings_on_space_type_id_and_space_id", unique: true
+    t.index ["space_type_id"], name: "index_space_type_mappings_on_space_type_id"
+  end
+
+  create_table "space_types", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "spaces", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.string "address"
+    t.string "nearest_station"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "feature_mappings", "features"
+  add_foreign_key "feature_mappings", "spaces"
+  add_foreign_key "space_type_mappings", "space_types"
+  add_foreign_key "space_type_mappings", "spaces"
+end
