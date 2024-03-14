580ab7e0fb44487069b9abffdc38db38bee3519b
underground0930 <go.nishiduka.1985@gmail.com>
2024-03-14 14:17:15 +0900

    トップの検索

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -2,6 +2,21 @@ class SpacesController < ApplicationController
 
   def index
     # 一覧を表示
+    @spaces = Space.all
+
+    if params[:space_type_id].present?
+      @spaces = @spaces.joins(:space_type_mappings).where(space_type_mappings: {space_type_id: params[:space_type_id]})
+    end
+
+    if params[:feature_ids].present?
+      @spaces = @spaces.joins(:feature_mappings).where(feature_mappings: {feature_id: params[:feature_ids]}).distinct
+    end
+
+    if params[:keyword].present?
+      @spaces = @spaces.where('spaces.name LIKE :keyword OR spaces.description LIKE :keyword OR spaces.address LIKE :keyword OR spaces.nearest_station LIKE :keyword',
+        keyword: '%#{params[:keyword]}%'
+      )
+    end
   end
 
   def new
--- app/models/space.rb
+++ app/models/space.rb
@@ -15,7 +15,7 @@
 class Space < ApplicationRecord
   has_many_attached :images
   has_many :space_type_mappings, dependent: :destroy
-  has_nany :space_types, through: :space_type_mappings
+  has_many :space_types, through: :space_type_mappings
   has_many :feature_mappings, dependent: :destroy
   has_many :features, through: :feature_mappings
 
