f2af89e0d55e330210135c3e0912c3c6a8bec5cd
underground0930 <go.nishiduka.1985@gmail.com>
2024-03-18 19:59:17 +0900

    update

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -19,6 +19,13 @@
 #
 FactoryBot.define do
   factory :user do
-    
+    email { "admin@example.com" }
+    password { "password"}
+    password_confirmation { "password"}
+    admin { false }
+
+    trait :admin do
+      admin { true }
+    end
   end
 end
--- spec/requests/spaces_spec.rb
+++ spec/requests/spaces_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Spaces", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /spaces" do
+    context "管理者ユーザーの場合" do
+      let(:user) { FactoryBot.create(:user, :admin)}
+
+      it "スペースの予約ができる" do
+        sign_in user
+        expect(user.admin?).to eq(true)
+        expect {
+          post spaces_path, params: { space: { name: "スペースA"}}
+        }.to change(Space, :count).by(1)
+        expect(response).to redirect_to(space_path(Space.last))
+      end
+    end
+    context "一般ユーザーの場合" do    
+      let(:user) { FactoryBot.create(:user)}
+      it "スペースの予約ができない" do
+        sign_in user
+        expect {
+          post spaces_path, params: { space: { name: "スペースA"}}
+        }.to change(Space, :count).by(0)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
   end
 end
